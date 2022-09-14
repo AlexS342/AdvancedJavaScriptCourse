162bd1d1b0f92cbe9d6923552447e60c93e4b4d1
AlexS <ashilov84@ya.ru>
2022-09-14 18:51:22 +0500

    добавил remove

--- lesson-7/server/cart.js
+++ lesson-7/server/cart.js
@@ -10,5 +10,15 @@ let change = (cart, req) => {
 
 module.exports = {
     add,
-    change
+    change,
+    remove
+};
+let remove = (cart, req) => {
+    let find = cart.contents.find(el => el.id_product === +req.params.id);
+    if (find.quantity > 1) {
+        find.quantity -= 1;
+    } else {
+        cart.contents.splice(cart.contents.indexOf(find), 1);
+    }
+    return JSON.stringify(cart, null, 4);
 };
